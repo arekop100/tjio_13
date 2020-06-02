2536b2bf4b09511e889287533d72b7d80da2feb2
arekop100 <arekop100@gmail.com>
2020-06-02 14:34:51 +0200

    lab 13 logic

--- src/main/java/pl/edu/pwsztar/ShoppingCart.java
+++ src/main/java/pl/edu/pwsztar/ShoppingCart.java
@@ -1,30 +1,95 @@
 package pl.edu.pwsztar;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShoppingCart implements ShoppingCartOperation {
 
+    private final List<Product> productList;
+
+    public ShoppingCart() {
+        this.productList = new ArrayList<>();
+    }
+
     public boolean addProducts(String productName, int price, int quantity) {
+        if(getAllProductsQuantity() + quantity > ShoppingCart.PRODUCTS_LIMIT || price <= 0 || quantity <= 0){
+            return false;
+        }
+        productName = productName.toLowerCase();
+
+        if(isProductAlreadyAdded(productName)){
+            for(Product product: productList){
+                if(product.getProductName().equals(productName) && product.getPrice() != price){
+                    product.setAmount(product.getAmount() + quantity);
+                    return true;
+                }
+            }
+        }else{
+            productList.add(new Product(productName, price, quantity));
+            return true;
+        }
         return false;
     }
 
     public boolean deleteProducts(String productName, int quantity) {
+        if(quantity <=0 ){
+            return false;
+        }
+        productName = productName.toLowerCase();
+        for(Product product: productList){
+            if(product.getProductName().equals(productName)){
+                if(product.getAmount() > quantity){
+                    product.setAmount(product.getAmount() - quantity);
+                    return true;
+                }else if(product.getAmount() == quantity){
+                    productList.remove(product);
+                    return true;
+                }
+
+            }
+        }
         return false;
     }
 
     public int getQuantityOfProduct(String productName) {
-        return 0;
+        return productList.stream()
+                .filter(product -> product.getProductName().equals(productName.toLowerCase()))
+                .mapToInt(Product::getAmount).sum();
     }
 
     public int getSumProductsPrices() {
-        return 0;
+       return productList.stream().mapToInt(product -> product.getPrice() * product.getAmount()).sum();
     }
 
     public int getProductPrice(String productName) {
+        productName = productName.toLowerCase();
+
+        for(Product product: productList){
+
+            if(product.getProductName().equals(productName)){
+                return product.getPrice();
+            }
+        }
         return 0;
     }
 
     public List<String> getProductsNames() {
-        return null;
+        return productList.stream().map(product ->
+                product.getProductName().substring(0,1).toUpperCase() + product.getProductName().substring(1))
+                .collect(Collectors.toList());
+
+    }
+
+    private boolean isProductAlreadyAdded(String name){
+        return productList.stream()
+                .anyMatch(product -> product.getProductName()
+                .equals(name));
+    }
+
+    private  int getAllProductsQuantity(){
+        return productList.stream()
+                .mapToInt(product -> product.getPrice() * product.getAmount())
+                .sum();
     }
 }
